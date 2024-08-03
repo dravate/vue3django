40277c84654c71cadf8c83fed2bb6964c0e21391
Sopan <sshewale@algoanalytics.com>
2024-08-03 14:08:58 +0530

    Session two files

---
+++ session2/front-end/sampleapp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <input v-model="message" placeholder="Edit me">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, Vue 3!'
+    }
+  }
+}
+</script>
+
